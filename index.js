a84595dae3326dafc0d51f3d309763298e042698
roushan-sheik <roushansheik@gmail.com>
2024-06-27 14:17:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const main = (str) => {
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+
+};
+const result = main("Hello");
+console.log(result);
